3452a1141caa84db48ef1aa6083c508026b310a6
YKDZ <69898831+YKDZ@users.noreply.github.com>
2025-08-04 22:50:59 +0800

    Use pino as logger

---
+++ apps/app/src/server/middleware/logger.ts
@@ -0,0 +1,24 @@
+import { createMiddleware } from "hono/factory";
+import { pinoHttp } from "pino-http";
+
+export const pinoLoggerMiddleware = createMiddleware(async (c, next) => {
+  c.env.incoming.id = c.var.requestId;
+
+  await new Promise<void>((resolve) =>
+    pinoHttp({
+      level: "debug",
+      quietResLogger: true,
+      quietReqLogger: true,
+      transport: {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+        },
+      },
+    })(c.env.incoming, c.env.outgoing, () => resolve()),
+  );
+
+  c.set("logger", c.env.incoming.log);
+
+  await next();
+});
