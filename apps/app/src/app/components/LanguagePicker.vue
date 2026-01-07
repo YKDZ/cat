754e585a77507465bffca0441e2a2877fd410635
YKDZ <3070799584@qq.com>
2026-01-07 19:08:55 +0800

    fix: picker

--- apps/app/src/app/components/LanguagePicker.vue
+++ apps/app/src/app/components/LanguagePicker.vue
@@ -3,14 +3,16 @@ import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import Picker from "@/app/components/picker/Picker.vue";
 import { orpc } from "@/server/orpc";
-import { useQuery } from "@pinia/colada";
+import { useInfiniteQuery } from "@pinia/colada";
 
 const props = withDefaults(
   defineProps<{
     filter?: (option: { value: string; content: string }) => boolean;
+    portal?: boolean;
   }>(),
   {
     filter: () => true,
+    portal: true,
   },
 );
 
@@ -19,18 +21,28 @@ const { t } = useI18n();
 const languageId = defineModel<string | undefined>();
 const search = ref("");
 
-const { state } = useQuery({
-  key: ["languages"],
-  query: () =>
+const { state, loadNextPage } = useInfiniteQuery({
+  key: () => ["languages", search.value],
+  query: ({ pageParam }) =>
     orpc.language.getAll({
       searchQuery: search.value,
+      page: Number(pageParam),
     }),
+  initialPageParam: 0,
+  getNextPageParam: (lastPage, _1, lastPageParam) => {
+    if (lastPage.hasMore) {
+      return Number(lastPageParam) + 1;
+    }
+    return undefined;
+  },
 });
 
 const options = computed(() => {
-  if (!state.value || !state.value.data) return [];
+  const pages = state.value?.data?.pages;
+  if (!pages) return [];
 
-  return state.value.data.languages
+  return pages
+    .flatMap((page) => page.languages)
     .map((language) => {
       return {
         content: t(language.id),
@@ -47,5 +59,7 @@ const options = computed(() => {
     v-model:search="search"
     :options
     :placeholder="t('选择一个语言...')"
+    :load-more="() => loadNextPage()"
+    :portal="props.portal"
   />
 </template>
