0cc7f852d7268279139e327bbf8a112493d0ec98
YKDZ <3070799584@qq.com>
2026-01-07 15:30:54 +0800

    chore: ws auto reconnect

--- apps/app/src/server/ws.ts
+++ apps/app/src/server/ws.ts
@@ -3,6 +3,7 @@ import { logger } from "@cat/shared/utils";
 import { createORPCClient, onError } from "@orpc/client";
 import { RPCLink } from "@orpc/client/websocket";
 import type { RouterClient } from "@orpc/server";
+import { WebSocket } from "partysocket";
 
 const link = new RPCLink({
   websocket: new WebSocket("ws://localhost:3000/api/ws"),
