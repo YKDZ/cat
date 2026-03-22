992bae4995acc431d4d837b20006e4a2ba2f57de
YKDZ <3070799584@qq.com>
2026-03-22 03:14:33 +0000

    chore: clean

--- packages/ui/src/types/vike.d.ts
+++ packages/ui/src/types/vike.d.ts
@@ -1,17 +1,3 @@
-import type { DrizzleDB, RedisConnection } from "@cat/db";
-import type { ComponentRecord, PluginManager } from "@cat/plugin-core";
-import type { User } from "@cat/shared/schema/drizzle/user";
-import type { HTTPHelpers } from "@cat/shared/utils";
-import type { Pinia, StateTree } from "pinia";
-import type { Component } from "vue";
-import type {
-  I18n,
-  LocaleMessageValue,
-  RemovedIndexResources,
-  VueMessageType,
-  Message,
-} from "vue-i18n";
-
 declare global {
   namespace Vike {
     interface PageContextServer {
